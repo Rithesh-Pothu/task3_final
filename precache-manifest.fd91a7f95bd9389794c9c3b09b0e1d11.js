a2a66685ba405a55b8e79fb438b48c8d3f184d03
Rithesh-Pothu <rithesh.441@gmail.com>
2020-10-08 11:48:53 +0530

    Updates

---
+++ precache-manifest.fd91a7f95bd9389794c9c3b09b0e1d11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf74119e72328b7a794dee7157772ce3",
+    "url": "/task3_final/index.html"
+  },
+  {
+    "revision": "0561935d497b126c2b44",
+    "url": "/task3_final/static/css/main.18869955.chunk.css"
+  },
+  {
+    "revision": "e56d8c61f9baf59941d0",
+    "url": "/task3_final/static/js/2.5dbd5c1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task3_final/static/js/2.5dbd5c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0561935d497b126c2b44",
+    "url": "/task3_final/static/js/main.48d265ac.chunk.js"
+  },
+  {
+    "revision": "5d72203dc04aefaa3df9",
+    "url": "/task3_final/static/js/runtime-main.e2cbabe5.js"
+  },
+  {
+    "revision": "4d7c5c4f9546c2806458fccfebc2246f",
+    "url": "/task3_final/static/media/Rithesh.4d7c5c4f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task3_final/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d42e5078f0717a065edcfb0976e71d0b",
+    "url": "/task3_final/static/media/purush.d42e5078.jpg"
+  }
+]);
